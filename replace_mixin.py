91cf6dfd83be0e71717eb3cf5d3a851aa48f30e4
Dan Rosén <danr42@gmail.com>
2022-08-30 10:46:21 +0200

    Update replace mixin to add log automatically

--- replace_mixin.py
+++ replace_mixin.py
@@ -122,8 +122,8 @@ class Select(Generic[P, R], PrivateReplaceMixin):
     def __iter__(self):
         yield from self.where() # type: ignore
 
-def coll(nonce: Any, *args: Any, **kws: Any):
-    for field, arg in zip(fields(nonce), args):
+def coll(cls: Any, *args: Any, **kws: Any):
+    for field, arg in zip(fields(cls), args):
         kws[field.name] = arg
     return kws.items()
 
@@ -142,12 +142,12 @@ class DB:
         def where(opts: SelectOptions, *args: P.args, **kws: P.kwargs) -> list[R]:
             clauses: list[str] = []
             for f, a in coll(t, *args, **kws):
-                clauses += [f"v -> {sqlquote(f)} = {sqlquote(json.dumps(a))} -> '$'"]
+                clauses += [f"value -> {sqlquote(f)} = {sqlquote(json.dumps(a))} -> '$'"]
             if clauses:
                 where_clause = 'where ' + ' and '.join(clauses)
             else:
                 where_clause = ''
-            stmt = f'select v from {t.__name__} {where_clause} order by v ->> {opts.order!r}'
+            stmt = f'select value from {t.__name__} {where_clause} order by value ->> {opts.order!r}'
             limit, offset = opts.limit, opts.offset
             if limit is None:
                 limit = -1
@@ -164,59 +164,99 @@ class DB:
 
 from dataclasses import asdict
 
-db: DB = ... # type: ignore
-
 from typing import Protocol
 import textwrap
 
 class DBMixin(ReplaceMixin):
     id: int
 
-    def delete(self) -> int:
-        tbl = self.__class__.__name__
-        c = db.con.execute(f'''
-            delete from {tbl} where id = ?
-        ''', [self.id])
-        return c.rowcount
-
-    def save(self) -> Self:
-        tbl = self.__class__.__name__
+    def save(self, db: DB) -> Self:
+        Table = self.__class__.__name__
+        meta = getattr(self.__class__, '__meta__', None)
         db.con.executescript(textwrap.dedent(f'''
-            create table if not exists {tbl} (
-              id integer as (v ->> 'id') unique,
-              v text,
-              check (typeof(id) = 'integer'),
-              check (id >= 0),
-              check (json_valid(v))
+            create table if not exists {Table} (
+                id integer as (value ->> 'id') unique,
+                value text,
+                check (typeof(id) = 'integer'),
+                check (id >= 0),
+                check (json_valid(value))
             );
-            create index if not exists {tbl}_id on {tbl} (id);
+            create index if not exists {Table}_id on {Table} (id);
         '''))
-        exists = db.con.execute(f'''
-            select 1 from {tbl} where id = ?
-        ''', [self.id]).fetchall()
+        if isinstance(meta, Meta) and meta.log:
+            LogTable = meta.log_table or f'{Table}Log'
+            exists = any(
+                db.con.execute(f'''
+                    select 1 from sqlite_master where type = "table" and name = ?
+                ''', [LogTable])
+            )
+            if not exists:
+                db.con.executescript(textwrap.dedent(f'''
+                    create table {LogTable} (
+                        t timestamp default (strftime('%Y-%m-%d %H:%M:%f', 'now', 'localtime')),
+                        action text,
+                        id integer,
+                        new json
+                    );
+                    create trigger
+                        {Table}_insert after insert on {Table}
+                    begin
+                        insert into {LogTable}(id, new, action) values (NEW.id, NEW.value, "insert");
+                    end;
+                    create trigger
+                        {Table}_update after update on {Table}
+                    begin
+                        insert into {LogTable}(id, new, action) values (OLD.id, NEW.value, "update");
+                    end;
+                    create trigger
+                        {Table}_delete after delete on {Table}
+                    begin
+                        insert into {LogTable}(id, new, action) values (OLD.id, NULL, "delete");
+                    end;
+                '''))
+        if self.id == -1:
+            exists = False
+        else:
+            exists = any(
+                db.con.execute(f'''
+                    select 1 from {Table} where id = ?
+                ''', [self.id])
+            )
         if exists:
             db.con.execute(f'''
-                update {tbl} set v = ? -> '$' where id = ?
+                update {Table} set value = ? -> '$' where id = ?
             ''', [json.dumps(asdict(self)), self.id])
             db.con.commit()
             return self
         else:
-            id, = db.con.execute(f'''
-                select ifnull(max(id) + 1, 0) from {tbl};
-            ''').fetchone()
-            res = self.replace(id=id) # type: ignore
+            if self.id == -1:
+                id, = db.con.execute(f'''
+                    select ifnull(max(id) + 1, 0) from {Table};
+                ''').fetchone()
+                res = self.replace(id=id) # type: ignore
+            else:
+                id = self.id
+                res = self
             db.con.execute(f'''
-                insert into {tbl} values (? -> '$')
+                insert into {Table} values (? -> '$')
             ''', [json.dumps(asdict(res))])
             db.con.commit()
             return res
 
-    @property
-    def update(self) -> Type[Self]:
-        return lambda *args, **kws: self.replace(*args, **kws).save() # type: ignore
+    def delete(self, db: DB) -> int:
+        Table = self.__class__.__name__
+        c = db.con.execute(f'''
+            delete from {Table} where id = ?
+        ''', [self.id])
+        return c.rowcount
 
-    def reload(self) -> Self:  # reload ?
+    def reload(self, db: DB) -> Self:
         return db.get(self.__class__).where(id=self.id)[0]
+
+@dataclass(frozen=True)
+class Meta:
+    log: bool = False
+    log_table: None | str = None
 
 @dataclass
 class Todo(DBMixin):
@@ -226,134 +266,121 @@ class Todo(DBMixin):
     # created: datetime = field(default_factory=lambda: datetime.now())
     id: int = -1
 
+    __meta__: ClassVar[Meta] = Meta(log=True)
+
 @dataclass
 class Person(DBMixin):
     name: str = ''
     parent_ids: list[int] = field(default_factory=list)
     id: int = -1
 
-    @property
-    def parents(self):
+    def parents(self, db: DB):
         return [
             p
             for pid in self.parent_ids
             for p in db.get(Person).where(id=pid)
         ]
 
+    __meta__: ClassVar[Meta] = Meta(log=True)
+
 from pprint import pp
 
-with sqlite3.connect(':memory:') as con:
-    db: DB = DB(con)
+def main():
+    with sqlite3.connect(':memory:') as con:
+        db: DB = DB(con)
 
-    Todos = db.get(Todo)
-    Persons = db.get(Person)
+        Todos = db.get(Todo)
+        Persons = db.get(Person)
 
-    Todo('banana').save()
-    Todo('flapp', done=True).save()
-    Todo('boop').save()
-    Todo('floop').save()
-    h = Todo('happ').save()
-    pp(list(db.con.execute("select * from Todo")))
-    pp(Todos.where(done=True))
-    pp(Todos.where(done=False))
-    pp(Todos.where(done=True, msg='flapp'))
-    pp(Todos.where(msg='banana'))
-    pp(Todos.where(id=2))
-    pp(h)
-    h = h.replace(msg='happ 2', done=True)
-    pp(h.save())
-    pp(list(db.con.execute("select * from Todo")))
-    pp(Todos.where())
-    pp(Todos.order('msg').where())
+        Todo('banana').save(db)
+        Todo('flapp', done=True).save(db)
+        Todo('boop').save(db)
+        Todo('floop').save(db)
+        h = Todo('happ').save(db)
+        pp(list(db.con.execute("select * from Todo")))
+        pp(Todos.where(done=True))
+        pp(Todos.where(done=False))
+        pp(Todos.where(done=True, msg='flapp'))
+        pp(Todos.where(msg='banana'))
+        pp(Todos.where(id=2))
+        pp(h)
+        h = h.replace(msg='happ 2', done=True)
+        pp(h.save(db))
+        pp(list(db.con.execute("select * from Todo")))
+        pp(Todos.where())
+        pp(Todos.order('msg').where())
 
-    print('---')
+        print('---')
 
-    dan = Person('Dan').save()
+        dan = Person('Dan').save(db)
 
-    db.con.executescript('''
-        create table PersonLog (
-          t timestamp default (strftime('%Y-%m-%d %H:%M:%f', 'now', 'localtime')),
-          action text,
-          id integer,
-          new json
-        );
-        create trigger
-            Person_insert after insert on Person
-        begin
-            insert into PersonLog(id, new, action) values (NEW.id, NEW.v, "insert");
-        end;
-        create trigger
-            Person_update after update on Person
-        begin
-            insert into PersonLog(id, new, action) values (OLD.id, NEW.v, "update");
-        end;
-        create trigger
-            Person_delete after delete on Person
-        begin
-            insert into PersonLog(id, new, action) values (OLD.id, NULL, "delete");
-        end;
-    ''')
+        dan.delete(db)
+        dan.save(db)
 
-    dan.delete()
-    dan.save()
+        unni = Person('Unni').save(db)
+        iris = Person('Iris', parent_ids=[dan.id, unni.id]).save(db)
 
-    unni = Person('Unni').save()
-    iris = Person('Iris', parent_ids=[dan.id, unni.id]).save()
+        pp([dan, unni, iris])
 
-    pp([dan, unni, iris])
+        pp([p for p in Persons])
+        pp(Persons.order('name').where())
 
-    pp([p for p in Persons])
-    pp(Persons.order('name').where())
+        pp(Persons.where(parent_ids=[]))
+        pp(Persons.where(parent_ids=[0, 1]))
 
-    pp(Persons.where(parent_ids=[]))
-    pp(Persons.where(parent_ids=[0, 1]))
+        ps: list[Person] = iris.parents(db)
+        pp(ps)
 
-    ps: list[Person] = iris.parents
-    pp(ps)
+        pp(list(db.con.execute("""
+            select
+                a.value, b.value
+            from
+                Person a,
+                json_each(a.value -> 'parent_ids') p,
+                Person b
+            where
+                b.id = p.value
+        """)))
 
-    pp(list(db.con.execute("""
-        select
-            a.v, b.v
-        from
-            Person a,
-            json_each(a.v -> 'parent_ids') p,
-            Person b
-        where
-            b.id = p.value
-    """)))
+        Persons.get(id=1).replace(name='Unni Björklund').save(db)
+        u = Persons.get(id=0)
+        u.delete(db)
+        u.delete(db)
+        u.save(db)
+        print(Persons.get(id=1).name)
 
-    Persons.get(id=1).update(name='Unni Björklund')
-    u = Persons.get(id=0)
-    u.delete()
-    u.delete()
-    print(Persons.get(id=1).name)
+        if 0:
+            def lens(value: str, update: Callable[[str], None], js_fragment: str='event.value'):
+                return {
+                    'value': value,
+                    'onchange': call(update, js(js_fragment))
+                }
 
-    if 0:
-        def lens(value: str, update: Callable[[str], None], js_fragment: str='event.value'):
-            return {
-                'value': value,
-                'onchange': call(update, js(js_fragment))
-            }
-
-        input(
-            lens(dan.name, lambda s: dan.update(name=s))
-        )
-
-    if 1:
-        from pathlib import Path
-        db.con.commit()
-        Path("lol.db").unlink(missing_ok=True)
-        db.con.execute('vacuum into "lol.db"')
-        # db.con.close()
-        from subprocess import check_output
-        print(
-            check_output(
-                [
-                    'sqlite3',
-                    'lol.db',
-                    '.mode box',
-                    'select *, new ->> "name" from PersonLog'
-                ],
-                encoding='utf8'
+            input(
+                lens(dan.name, lambda s: dan.update(name=s))
             )
-        )
+
+        if 1:
+            from pathlib import Path
+            db.con.commit()
+            Path("lol.db").unlink(missing_ok=True)
+            db.con.execute('vacuum into "lol.db"')
+            # db.con.close()
+            from subprocess import check_output
+            print(
+                check_output(
+                    [
+                        'sqlite3',
+                        'lol.db',
+                        '.mode box',
+                        'select *, value ->> "name" as "name" from Person;'
+                        'select *, value ->> "msg" as msg, value ->> "done" as done from Todo;'
+                        'select *, new ->> "name" from PersonLog;'
+                        'select *, new ->> "msg" as msg, new ->> "done" as done, new ->> "$.id" from TodoLog;'
+                    ],
+                    encoding='utf8'
+                )
+            )
+
+if __name__ == '__main__':
+    main()
