24293563652bb1dc1a85be72a1f0c8d345f3275f
Dan Rosén <danr42@gmail.com>
2022-12-02 13:41:11 +0100

    Add --glob and --clear

--- vire/vire.py
+++ vire/vire.py
@@ -10,42 +10,50 @@ import signal
 import sys
 import threading
 import tty
-import os
 import argparse
-import textwrap
 
 from inotify_simple import INotify, masks, flags
 
-def run(argv: list[str], is_module: bool):
-    sys.stderr = sys.stdout
+def run(argv: list[str], is_module: bool, clear_opt: int):
     sys.argv[1:] = argv[1:]
+    clear(clear_opt)
     if is_module:
         runpy.run_module(argv[0], run_name='__main__')
     else:
         runpy.run_path(argv[0], run_name='__main__')
     print('\nvire: child done.')
 
-def main_inner(preload: str, argv: list[str], is_module: bool):
+def sigterm(pid: int):
+    try:
+        os.kill(pid, signal.SIGTERM)
+    except ProcessLookupError:
+        pass
+
+def clear(clear_opt: int):
+    # from entr: https://github.com/eradman/entr/blob/master/entr.c
+    # 2J - erase the entire display
+    # 3J - clear scrollback buffer
+    # H  - set cursor position to the default
+    if clear_opt == 1:
+        print('\033[2J\033[H', flush=True)
+    if clear_opt >= 2:
+        print('\033[2J\033[3J\033[H', flush=True)
+
+def main_inner(preload: str, argv: list[str], is_module: bool, glob_pattern: str, clear_opt: int=0):
     for name in preload.split(','):
         importlib.import_module(name.strip())
     ino = INotify()
-    for name in Path('.').glob('**/*py'):
-        ino.add_watch(name, flags.CLOSE_WRITE)
+    for name in Path('.').glob(glob_pattern):
+        ino.add_watch(name, flags.MODIFY)
     while True:
-        pid = fork(lambda: run(argv, is_module=is_module))
+        pid = fork(lambda: run(argv, is_module=is_module, clear_opt=clear_opt))
         try:
             events = ino.read(read_delay=5)
         except KeyboardInterrupt:
-            print('\nvire: parent recevied ^C, quitting.')
-            try:
-                os.kill(pid, signal.SIGTERM)
-            except ProcessLookupError:
-                pass
+            print('\nvire: parent received ^C, quitting.')
+            sigterm(pid)
             quit()
-        try:
-            os.kill(pid, signal.SIGTERM)
-        except ProcessLookupError:
-            pass
+        sigterm(pid)
         # print(events, flush=True)
 
 def spawn(f: Callable[[], None]) -> None:
@@ -81,20 +89,36 @@ def fork(child: Callable[[], None]):
 
 def main():
     parser = argparse.ArgumentParser(
-        prog='vire',
-        description=textwrap.dedent('''
-            Runs a program and reruns it when any files matching **/*py is updated.
-            Recommended: run with PYTHONDONTWRITEBYTECODE=x
-        ''')
+        description='''
+            Runs a program and reruns it on updating files matching a glob (default **/*.py).
+        ''',
+        epilog='''
+            Recommended: run with PYTHONDONTWRITEBYTECODE=x.
+            This prevents a race condition where outdated .pyc files are run.
+        ''',
+        add_help=False,
     )
-    parser.add_argument('-p', '--preload', help='Modules to preload, comma-separated. Example: "flask,pandas"')
-    parser.add_argument('-m', help='Argument is a module, will be run like python -m (using runpy)', action='store_true')
+    parser.add_argument('--help', '-h', action='store_true', help=argparse.SUPPRESS)
+    parser.add_argument('--clear', '-c', action='count', default=0, help='Clear the screen before invoking the utility. Specify twice to erase the scrollback buffer.')
+    parser.add_argument('--preload', '-p', metavar='M', help='Modules to preload, comma-separated. Example: flask,pandas')
+    parser.add_argument('--glob', '-g', metavar='G', help='Watch for updates to files matching this glob, Default: **/*.py', default='**/*.py')
+    parser.add_argument('-m', action='store_true', help='Argument is a module, will be run like python -m (using runpy)')
     parser.add_argument(dest='argv', nargs=argparse.REMAINDER)
     args = parser.parse_args()
 
+    if not args.argv or args.help:
+        parser.print_help()
+        quit()
+
     mode = tty.tcgetattr(STDIN_FILENO)
     try:
-        main_inner(args.preload, args.argv, is_module=args.m)
+        main_inner(
+            args.preload,
+            args.argv,
+            is_module=args.m,
+            glob_pattern=args.glob,
+            clear_opt=args.clear,
+        )
     finally:
         tty.tcsetattr(STDIN_FILENO, tty.TCSAFLUSH, mode)
 
